754b0b5401c9620d7faf90d5516a846a5e6202cb
Christophe BENARD <ch.benard@gmail.com>
2020-11-26 09:35:38 +0100

    server/app.js updated - echoing hostname and port used by Node when Node is launched

--- server/app.js
+++ server/app.js
@@ -1,13 +1,13 @@
 const loaders = require('#server/loaders');
 const { port } = require('#server/config');
-
+const { backUrl } = require('#server/config');
 module.exports = {
     start() {
         const app = loaders.express();
         loaders.routes(app);
 
         app.listen(port, () => {
-            console.log(`Server is now running on port ${port}! :)`);
+            console.log(`Node is now running the API on ${backUrl}! :)`);
         });
     },
 };
